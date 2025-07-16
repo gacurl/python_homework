78d85803cc663655d8b7e982e19b7596a5553ab6
gacurl <greg.a.curl@gmail.com>
2025-07-16 15:51:21 -0400

    Complete tasks 4-5 and stop at 6

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -28,11 +28,29 @@ def calc(x, y, operation='multiply'):
     else:
         return "Operation not recognized."
 # Task 4: Data Type Conversion
-def data_type_conversion(value, name):
-    if name == 'int':
-        return int(value)
-    elif name == 'float':
-        return float(value)
-    elif name == 'str':
-        return str(value)
-           
+def data_type_conversion(name, value):
+    try:
+        if value == 'int':
+            return int(name)
+        elif value == 'float':
+            return float(name)
+        elif value == 'str':
+            return str(name)
+    except ValueError:
+        return f"You can't convert {name} into a {value}."
+# Task 5: Grading System, Using *args
+def grade(*args):
+    try:
+        average = sum(args) / len(args)
+        if average >= 90:
+            return 'A'
+        elif average >= 80:
+            return 'B'
+        elif average >= 70:
+            return 'C'
+        elif average >= 60:
+            return 'D'
+        else:
+            return 'F'
+    except:
+        return "Invalid data was provided."
