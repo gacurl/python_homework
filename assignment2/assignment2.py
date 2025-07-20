45e546869e26455da2b7bb71267b059ebd8df99e
gacurl <greg.a.curl@gmail.com>
2025-07-20 09:50:27 -0400

    add diary files and tasks 2-5. start task 6

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -35,3 +35,31 @@ def read_employees():
 
 employees = read_employees()
 print(employees)
+
+# Task 3: Find the Column Index
+def column_index(column_name):
+    # Look for position of column_name
+    return employees["fields"].index(column_name)
+
+employee_id_column = column_index("employee_id")
+
+# Task 4: Find the Employee First Name
+def first_name(row_number): # a cell number - need column and row
+    # use column index to find column number for "first_name"
+    first_name_column_index = column_index("first_name")
+    # get the row at the given row_number parameter
+    first_name_row = employees["rows"][row_number]
+    # Return that first name
+    return first_name_row[first_name_column_index]  # extract just the first name
+
+# Task 5: Find the Employee: a Function in a Function
+def employee_find(employee_id):
+    # define employee_match(row):
+    def employee_match(row): # helper to filter rows
+        return int(row[employee_id_column]) == employee_id # checks if this row is the one (True if matches - note the type conversion)
+    # use filter to apply employee_match to employees["rows"]
+    matches = list(filter(employee_match, employees["rows"])) # returns only rows that match the given ID and convert result to list
+    return matches
+
+
+
