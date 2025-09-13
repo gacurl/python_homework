008c1038f4b32b3d5059c7f300d1919f8634fae1
gacurl <greg.a.curl@gmail.com>
2025-09-13 19:48:02 -0400

    Complete Task 3

--- assignment10/get_books.py
+++ assignment10/get_books.py
@@ -8,12 +8,70 @@
     # - User-agent section used: *
     # - Disallow: /v2/availability/ but no issues for /v2/search/
     # - Crawl-delay: 120
-    # - OK to fetch a single results page with 2s delay? Yes
+    # - OK to fetch a single results page with 2s delay? No - need 120 seconds
 
 # Task 2: Understanding HTML and the DOM for the Durham Library Site
-# # ----- selectors from DOM -----
+# ----- ----- ----- selectors from DOM ----- ----- -----
 # CSS_RESULT_LI = "li.row.cp-search-result-item"
 # CSS_TITLE = "h2.cp-title .title-content"
 # CSS_AUTHOR_LINKS = ".cp-by-author-block a"
 # CSS_FMT_YEAR_WRAPPER = ".cp-format-info .display-info"
 # CSS_FMT_YEAR_SPAN = ".display-info-primary"
+
+# Task 3: Write a Program to Extract this Data
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import pandas as pd
+import json
+
+opts = webdriver.ChromeOptions()
+opts.add_argument('--headless')
+
+driver = webdriver.Chrome(options=opts)
+
+SEARCH_URL = "https://durhamcounty.bibliocommons.com/v2/search?query=learning%20spanish&searchType=smart"
+CSS_RESULT_LI           = "li.row.cp-search-result-item"
+CSS_TITLE               = "h2.cp-title .title-content"
+CSS_AUTHOR_LINKS        = ".cp-by-author-block a"
+CSS_FMT_YEAR_WRAPPER    = ".cp-format-info .display-info"
+CSS_FMT_YEAR_SPAN       = ".display-info-primary"
+
+driver.get(SEARCH_URL)
+# print("Title: ", driver.title)
+cards = driver.find_elements(By.CSS_SELECTOR, CSS_RESULT_LI)
+print("cards found: ", len(cards))
+sleep(5)
+results = []
+
+for li in cards:
+    # Title
+    try:
+        title = li.find_element(By.CSS_SELECTOR, CSS_TITLE).text.strip()
+    except Exception:
+        title = ""
+
+    # Author
+    authors = []
+    for a in li.find_elements(By.CSS_SELECTOR, CSS_AUTHOR_LINKS):
+        txt = (a.text or "").strip()
+        if txt:
+            authors.append(txt)
+    author_string = "; ".join(authors)
+
+    #Format-Year
+    try:
+        wrapper = li.find_element(By.CSS_SELECTOR, CSS_FMT_YEAR_WRAPPER)
+        format_year = wrapper.find_element(By.CSS_SELECTOR, CSS_FMT_YEAR_SPAN).text.strip()
+    except Exception:
+        format_year = ""
+
+    results.append({"Title": title, "Authors": author_string, "Format-Year": format_year})
+
+# print("rows built: ", len(results))
+# print(results)
+df = pd.DataFrame(results, columns=["Title", "Authors", "Format-Year"])
+print(df.head(10))
+
+driver.quit()
+
